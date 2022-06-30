977ce61b299ee43aa0a2758f5fe0879ac991fb28
Denis Nikolaev <dfs.str@gmail.com>
2022-06-30 15:37:24 +0500

    added vault and send grid settings

--- HappyTravel.Aoba/Infrastructure/WebApplicationBuilderExtensions.cs
+++ HappyTravel.Aoba/Infrastructure/WebApplicationBuilderExtensions.cs
@@ -1,3 +1,8 @@
+using HappyTravel.Aoba.Services;
+using HappyTravel.MailSender;
+using HappyTravel.MailSender.Infrastructure;
+using HappyTravel.MailSender.Models;
+using HappyTravel.VaultClient;
 using NATS.Client;
 
 namespace HappyTravel.Aoba.Infrastructure;
@@ -6,6 +11,8 @@ public static class WebApplicationBuilderExtensions
 {
     public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
     {
+        #region Add Nats
+
         var endpoints = builder.Configuration.GetValue<string>("Nats:Endpoints").Split(";");
         
         builder.Services.AddNatsClient(options =>
@@ -13,8 +20,35 @@ public static class WebApplicationBuilderExtensions
             options.Servers = endpoints;
             options.MaxReconnect = Options.ReconnectForever;
         }, ServiceLifetime.Singleton);
+
+        #endregion
+
+        #region Add SendGrid sender
+
+        using var vaultClient = new VaultClient.VaultClient(new VaultOptions
+        {
+            Engine = builder.Configuration["Vault:Engine"],
+            Role = builder.Configuration["Vault:Role"],
+            BaseUrl = new Uri(builder.Configuration[builder.Configuration["Vault:Endpoint"]])
+        });
+        vaultClient.Login(builder.Configuration[builder.Configuration["Vault:Token"]]).Wait();
         
-        // TODO: add vault and mail sender settings
+        var mailSettings = vaultClient.Get(builder.Configuration["MailOptions"]).GetAwaiter().GetResult();
+        builder.Services.Configure<SenderOptions>(options =>
+        {
+            options.ApiKey = mailSettings["apiKey"];
+            options.SenderAddress = new EmailAddress(mailSettings["senderAddress"]);
+            options.BaseUrl = new Uri(mailSettings["baseUrl"]);
+        });
+
+        builder.Services.AddSingleton<IMailSender, SendGridMailSender>();
+        builder.Services.AddHttpClient(SendGridMailSender.HttpClientName)
+            .SetHandlerLifetime(TimeSpan.FromMinutes(5));
+
+        #endregion
+
+        builder.Services.AddHealthChecks();
+        builder.Services.AddHostedService<MessageBusConsumer>();
 
         return builder;
     }
