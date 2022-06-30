3bb01fb10821603d464e268c32b02ce0646300d5
Denis Nikolaev <dfs.str@gmail.com>
2022-06-30 14:42:30 +0500

    added endpoint mapping

--- HappyTravel.Aoba/Program.cs
+++ HappyTravel.Aoba/Program.cs
@@ -1,5 +1,6 @@
 using HappyTravel.Aoba.Infrastructure;
 using HappyTravel.Infrastructure.Extensions;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,4 +11,13 @@ builder.ConfigureInfrastructure(options =>
 builder.ConfigureServices();
 
 var app = builder.Build();
+
+app.UseRouting()
+    .UseHttpMetrics()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapMetrics().RequireHost($"*:{builder.Configuration.GetValue<int>("HTDC_METRICS_PORT")}");
+        endpoints.MapHealthChecks("/health").RequireHost($"*:{builder.Configuration.GetValue<int>("HTDC_HEALTH_PORT")}");
+    });
+
 app.Run();
